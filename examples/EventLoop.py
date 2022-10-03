93433864e829f2d7a8b686dc3585fe74db2d0c59
james lay <james@eventrentalsystems.com>
2022-10-03 12:27:04 -0600

    add 2nd example, colors

--- examples/EventLoop.py
+++ examples/EventLoop.py
@@ -1,4 +1,16 @@
 import time
+
+# create constants for different terminal colors:
+RED = "\033[1;31m"
+GREEN = "\033[1;32m"
+YELLOW = "\033[1;33m"
+BLUE = "\033[1;34m"
+PURPLE = "\033[1;35m"
+CYAN = "\033[1;36m"
+WHITE = "\033[1;37m"
+RESET = "\033[0;0m"
+GREY = "\033[1;30m"
+
 class TimeoutCallback:
     def __init__(self, callback, timeoutid, delay):
         self.callback = callback
@@ -35,5 +47,6 @@ class EventLoop():
                 self.running = False
             time.sleep(0.5)
             self.seconds += 0.5
-            print(f"tick: {self.seconds}s")
+            print(f"{CYAN}tick: {BLUE}{self.seconds}s{RESET}")
+
 
